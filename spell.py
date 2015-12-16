117b6bf2daaa91f0c8f297924c976ea39cc504a8
Dimitar Atanasov <dimitar.atanasof@gmail.com>
2015-12-16 21:11:21 +0200

    adding first version of files

---
+++ spell.py
@@ -0,0 +1,7 @@
+class Spell:
+    def __init__(self, name='Fireball', damage=30, mana_cost=50, cast_range=2):
+        self.name = name
+        self.damage = damage
+        self.mana_cost = mana_cost
+        self.cast_range = cast_range
+
